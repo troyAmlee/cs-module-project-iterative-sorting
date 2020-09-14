feb24b1879ea2935483ca924674794dcb6180204
troyAmlee <troyamlee19@gmail.com>
2020-09-14 15:04:21 -0500

    [MVP] All tests pass, implemented without looking at pseudocode

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,6 +4,14 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        for j in arr[i:]:
+            if(arr[smallest_index] > j):
+                smallest_index = arr.index(j)
+
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
+
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
@@ -14,13 +22,46 @@ def selection_sort(arr):
 
     return arr
 
+a = [45, 37, 66, 89, 54, 81, 2, 1, 7]
+
+print(f"Selection Sort: {selection_sort(a)}")
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    first_index = 0
+    second_index = 0
+    count = True
+    counted = 0
+    if(len(arr) == 0):
+        return arr
+    while(count == True):
+        for i,j in enumerate(arr):
+            if(i + 1 != len(arr)):
+                first_index = i
+                second_index = i + 1
+                if(arr[second_index] < arr[first_index]):
+                    temp = arr[first_index]
+                    arr[i] = arr[second_index]
+                    arr[second_index] = temp
+                    counted = counted + 1
+            elif(i == len(arr) - 1):
+                if(counted > 0):
+                    count = True
+                    counted = 0
+                else:
+                    count = False
+                    break
+            else:
+                break
+
 
 
     return arr
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+print(f"Bubble Sort: {bubble_sort(arr1)}")
 
 '''
 STRETCH: implement the Counting Sort function below
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,35 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in arr:
+        if i == target:
+            return arr.index(i)
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    
+    # find the midpoint element
+    # length//2
+    while(left <= right):
+        midpoint = (right + left) // 2
+        if(arr[midpoint] == target):
+            return midpoint
+        elif(arr[midpoint] > target):
+            right = midpoint - 1
+        elif(arr[midpoint] < target):
+            left = midpoint + 1
+    return -1
 
-    # Your code here
+a = [3, 4, 6, 16, 26, 28, 55, 56]
+
+print(binary_search(a, 55))
+print("-------")
+print(binary_search(a, 100))
 
 
-    return -1  # not found
+
+    
